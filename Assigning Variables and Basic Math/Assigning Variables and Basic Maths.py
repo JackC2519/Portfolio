de848dc73d2d8f468d8bdcc841ec097788c468a7
JackC2519 <blortorg@live.com.au>
2025-04-13 19:42:17 +1000

    Add files via upload

---
+++ Assigning Variables and Basic Math/Assigning Variables and Basic Maths.py
@@ -0,0 +1,31 @@
+# Assigning a value to variables A and B
+A = 7
+B = 3
+
+# Addition of 2 variables
+Add = A + B
+print(Add)
+
+# Subtraction of 2 variables 
+Subtract = A - B
+print(Subtract)
+
+# Multiplication of 2 variables
+Multiply = A * B
+print (Multiply)
+
+# Division of 2 variables
+Divide = A / B 
+print(Divide)
+
+# Rounding of the division of 2 variables
+Round = A // B
+print(Round)
+
+# showcasing the order of operations of a mathemtic equation
+Bodmas = (A * B) + 2
+print(Bodmas)
+
+# Raising 1 variable to the power of another
+Power = A ** B
+print(Power)
